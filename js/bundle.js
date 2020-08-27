5797e08ddc55a93c6b7d70db905fc3b6beb81b4c
Matt Difford <matt@mrsavvy.co.uk>
2020-08-27 14:06:18 +0100

    Optimisation changes

--- js/bundle.js
+++ js/bundle.js
@@ -3,8 +3,6 @@
 
 {% include_relative  src/jquery.min.js %}
 {% include_relative  src/modernizr.min.js %}
-{% include_relative  src/fullpagescroll.min.js %}
 {% include_relative  src/script.js %}
 {% include_relative  src/lazysizes.min.js %}
-{% include_relative  src/parsley.min.js %}
 {% include_relative  src/slick.min.js %}
